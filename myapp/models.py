92b0317740d2ab5d1ba326e4fbd0924cceb3e544
PRIYADHARSHINI G <gpriyampt@gmail.com>
2026-01-07 18:17:34 +0530

    Initial commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.core.validators import MinLengthValidator,RegexValidator
 class User(models.Model):
     id=models.AutoField(primary_key=True)
     username=models.CharField(max_length=100)
     email=models.EmailField(blank=False,unique=True)
-    phone_number=models.CharField(max_length=15,blank=False)
+    phone_number = models.CharField(
+    max_length=10,
+    validators=[
+        MinLengthValidator(10, message="Phone number must be 10 digits"),
+        RegexValidator(r'^\d{10}$', message="Phone number must contain only digits")
+    ]
+)
     password=models.CharField(max_length=100,blank=False)
     def __str__(self):
         return self.username
